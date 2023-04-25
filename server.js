d1dc8f3e1c98fc2a4309824c967eade99ef6420c
ThejasDevadiga <4al20ai047@gmail.com>
2023-04-25 11:30:50 +0530

    created a products model || inserted some data

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-
+const Products = require("./src/models/products");
+const asyncHandler = require('express-async-handler')
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb://localhost:27017', {
+    const conn = await mongoose.connect('mongodb://localhost:27017/HackToFuture', {
       useUnifiedTopology: true,
       useNewUrlParser: true,
     });
@@ -13,6 +14,12 @@ const connectDB = async () => {
     console.error(`Error: ${err.message}`);
     process.exit();
   }
+};
+const errorHandler = (err,req, res, next) => {
+    const statusCode = res.statusCode === 200 ? 400 : res.statusCode;
+    console.log(err.message);
+    // res.render('notFound', { title: 'ERROR PAGE', message: err.message ,heading:"ERROR FOUND"})
+    res.status(statusCode).json({ message: err.message,heading:"ERROR FOUND"})
 };
 
 connectDB();
@@ -31,9 +38,60 @@ console.log(__dirname);
 app.use(express.static('public'));  
 
 
+app.get("/products",asyncHandler(async(req,res)=>{
+    const result = await Products.find({},{})
+    res.status(200).json({
+    data:result})
+}))
+
+
+const insertData=async()=>{
+    await Products.insertMany([
+    {
+        "productName": "Book",
+        "productId": "S1",
+        "productCategory": "Stationary"
+    },
+    {
+        "productName": "Pen",
+        "productId": "S2",
+        "productCategory": "Stationary"
+    },
+    {
+        "productName": "Eraser",
+        "productId": "S3",
+        "productCategory": "Stationary"
+    },
+    {
+        "productName": "Pencil",
+        "productId": "S4",
+        "productCategory": "Stationary"
+    },
+    {
+        "productName": "Sanitizer",
+        "productId": "H1",
+        "productCategory": "Hyg"
+    },
+    {
+        "productName": "Drone",
+        "productId": "5",
+        "productCategory": "sdfsd"
+    },
+      
+      ])
+}
+  insertData()
+
+
+app.get("/",(req,res)=>{
+
+    res.render('home',{})
+})
+
+app.use(errorHandler);
 
 const PORT = 5000;
-console.log(`http://localhost:${PORT}/test`);
+console.log(`http://localhost:${PORT}/`);
 
 app.listen(PORT, console.log(`Server port ${PORT}`));
 
